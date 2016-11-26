7fcef2c2f9fc095ca28de1ed3761cd6c3d8d5432
jspc <james@zero-internet.org.uk>
2016-11-26 14:55:58 +0000

    Hook libraries together

--- migrate.rb
+++ migrate.rb
@@ -3,6 +3,10 @@
 require 'json'
 require 'optparse'
 
+require_relative 'lib/config'
+require_relative 'lib/db'
+require_relative 'lib/images'
+
 config_file = File.join( Dir.home, '.config', 'publisher.json')
 post = 1
 
@@ -15,3 +19,17 @@ OptionParser.new do|opts|
     config_file = c
   end
 end.parse!
+
+config = JSON.parse File.read(config_file)
+staging = Config.new(config['envs']['staging'])
+production = Config.new(config['envs']['production'])
+
+# dump staging post
+staging_db = DB.new staging.username, staging.password, staging.host, staging.port, staging.database
+production_db = DB.new production.username, production.password, production.host, production.port, production.database
+
+post = staging_db.dump post
+
+post['image'] = Images.new(staging.uri, post['image']).work
+
+production_db.insert post
